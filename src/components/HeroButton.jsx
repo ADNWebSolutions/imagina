ec3c8bf4c9e06ac9d11a405e8668095341a9ba63
Kapee87 <kapserker@gmail.com>
2023-11-13 16:44:11 -0300

    pequeño fix al hero y los links del nav final

--- src/components/HeroButton.jsx
+++ src/components/HeroButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroButton = () => {
   return (
     <button className='border border-amber-800 rounded-md hover:bg-[#FFBF00] p-1'>
-        <a href="/about">
+        <a href="#about">
           {/* /about = createBrowserRouter */}
             Sobre nosotros!
         </a>
