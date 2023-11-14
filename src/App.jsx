d1457f5773c2d051e4eec3ff2838076f09ba2121
arielLevita <levita.ariel@gmail.com>
2023-11-14 10:42:14 -0300

    qué ofrecemos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
+import WhatWeOffer from './components/WhatWeOffer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <WhatWeOffer />
       <Footer />
     </>
 
---
+++ src/components/WhatWeOffer.jsx
@@ -0,0 +1,29 @@
+const WhatWeOffer = () => {
+    return (
+        <div className="p-8" id="whatweoffer">
+            <h2 className="text-4xl my-8">¿Qué ofrecemos?</h2>
+            <div className="content">
+                <div className="img-acts-1 m-[auto]">
+                    <img className="rounded-2xl" src="../../public/act1.jpg" alt="" />
+                </div>
+                <div className="p-acts-1 flex justify-center items-center text-xl px-12">
+                    <p>Hemos innovado con una nueva propuesta pedagógica en la escuela de verano, llevada a cabo y coordinada por docentes del Colegio Jockey Club Córdoba y por profesores de los distintos deportes del club.</p>
+                </div>
+                <div className="p-acts-2 flex justify-center items-center text-xl px-12">
+                    <p>Hemos innovado con una nueva propuesta pedagógica en la escuela de verano, llevada a cabo y coordinada por docentes del Colegio Jockey Club Córdoba y por profesores de los distintos deportes del club.</p>
+                </div>
+                <div className="img-act-2 m-[auto]">
+                    <img className="rounded-2xl" src="../../public/act2.jpg" alt="" />
+                </div>
+                <div className="img-acts-3 m-[auto]">
+                    <img className="rounded-2xl" src="../../public/act3.jpg" alt="" />
+                </div>
+                <div className="p-acts-3 flex justify-center items-center text-xl px-12">
+                    <p>Hemos innovado con una nueva propuesta pedagógica en la escuela de verano, llevada a cabo y coordinada por docentes del Colegio Jockey Club Córdoba y por profesores de los distintos deportes del club.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WhatWeOffer
