8bcc6d7fc2f960cc48fc65292606fcee2540e9fc
Marcos <marcos_maidana@live.com>
2023-11-16 14:30:51 -0300

    Hero/banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,23 @@
 import HeroButton from "./HeroButton"
+import SocialMedia from "./SocialMedia"
 
 const Hero = () => {
   return (
-    <section className='heroBg h-[60vh] flex justify-center items-center '>
-            {/* img */}
-            <HeroButton />
-    </section>
-  )
+  <section className="h-screen flex flex-col justify-center items-center bg-cover bg-center text-white">
+      <div
+        className="absolute left-0 w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: 'url(./heroImg.png)' }}
+      ></div>
+      <div className="flex justify-between items-center w-[80%] md:w-2/3">
+        <div className="glassmorphic-box border p-4 rounded-lg text-center shadou ">
+          <h1 className="text-6xl md:text-7xl font-extrabold mb-2">Imagina</h1>
+          <p className="text-2xl md:text-2xl mb-8">Escuela de Verano </p>
+          <HeroButton />
+          <SocialMedia className="mt-4 flex justify-center items-center text-4xl" />
+        </div>
+      </div>
+
+    </section>)
 }
 
 export default Hero
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
+const SocialMedia = (props) => {
+    const className = props.className;
+    return (
+      <div className={`${className}`} >
+          <a href="https://www.instagram.com/arrecife_imagina/" target="_blank" className="hover:duration-200 hover:text-ocean1">
+            <FontAwesomeIcon icon={faInstagram} /> 
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100063487303709" target="_blank" className="mx-4 hover:duration-200 hover:text-ocean1">
+            <FontAwesomeIcon icon={faFacebook} /> 
+          </a>
+          <a href="https://wa.me/5493516533455?text=Buenos%20%20dias!,%20" target="_blank" className="hover:duration-200 hover:text-ocean1">
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </a>
+      </div>
+    )
+  }
+  
+  export default SocialMedia
