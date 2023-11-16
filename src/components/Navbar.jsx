fc426553b6aa9061b57a1dde4f63db77141061df
Kapee87 <kapserker@gmail.com>
2023-11-16 16:00:27 -0300

    added responsive to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 export default function Navbar() {
     return (
         <>
-            <div className="navbar bg-sand text-white">
+            <header className="navbar bg-sand text-white flex-col md:flex-row">
                 <div className="flex-1">
                     <img src="./LogoSinBg.png" alt="Imagina's logo" className='h-[40px]' />
                     <p className="btn btn-ghost normal-case text-xl">Imagina</p>
                 </div>
-                <div className="flex-none">
-                    <ul className="menu menu-horizontal px-1">
+                <nav className="flex">
+                    <ul className="flex justify-center gap-2 flex-wrap md:menu md:menu-horizontal px-1 ">
                         <li><a href='/'>Inicio</a></li>
                         <li><a href='#whatweoffer'>Nosotros</a></li>
                         <li><a href='#activities'>Actividades</a></li>
                         <li><a href='#contact'>Contacto</a></li>
                     </ul>
-                </div>
-            </div>
+                </nav>
+            </header>
         </>
     )
 }
