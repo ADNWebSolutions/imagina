0e5f70a2829fe5342e8ed8bb18083a7555516e61
Javier Castro <yoprogramo125@gmail.com>
2023-12-13 15:26:41 -0300

    correccion de alineado en footer, agregado de contacto en footer

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,20 +2,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 const SocialMedia = (props) => {
-    const className = props.className;
-    return (
-      <div className={`${className}`} >
+  const className = props.className;
+  return (
+    <>
+      <div className="">
+        <div>
+          <h2>Contacto</h2>
+          <p>351 653-3455</p>
+          <p>3572 50-4488</p>
+          <p>serenakpa6@hotmail.com</p>
+        </div>
+        <div className={`${className}`} >
           <a href="https://www.instagram.com/arrecife_imagina/" target="_blank" className="hover:duration-200 hover:text-sand">
-            <FontAwesomeIcon icon={faInstagram} /> 
+            <FontAwesomeIcon icon={faInstagram} />
           </a>
           <a href="https://www.facebook.com/profile.php?id=100063487303709" target="_blank" className="mx-4 hover:duration-200 hover:text-sand">
-            <FontAwesomeIcon icon={faFacebook} /> 
+            <FontAwesomeIcon icon={faFacebook} />
           </a>
           <a href="https://wa.me/5493516533455?text=Buenos%20%20dias!,%20" target="_blank" className="hover:duration-200 hover:text-sand">
             <FontAwesomeIcon icon={faWhatsapp} />
           </a>
+        </div>
       </div>
-    )
-  }
-  
-  export default SocialMedia
+    </>
+  )
+}
+
+export default SocialMedia
--- src/components/WhatWeOffer.jsx
+++ src/components/WhatWeOffer.jsx
@@ -37,10 +37,10 @@ const WhatWeOffer = () => {
                     {/* Pasamos por props el indice , el estado, y las funciones para ir cambiando las imagenes */}
                     <Carousel carousel={"first"} imgArray={imgArray} handleNext={handleNext} handlePrev={handlePrev} />
                     <div className="p-acts-1 flex justify-center items-center text-xl px-4 sm:px-12">
-                        <p>Nuestra historia, una hermosa travesía, comenzó hace 34 años bajo la inspiradora dirección de la señora Roxanna Taborda. Desde entonces, estamos comprometidos en ofrecer una muchas actividades que perduran como base de nuestra identidad. Entre las actividades incluimos la enseñanza de natación, momentos recreativos, experiencias en contacto con la naturaleza y mucho más.</p>
+                        <p>Nuestra historia, una hermosa travesía, comenzó hace 34 años bajo la inspiradora dirección de la seño Roxanna Taborda. Desde entonces, estamos comprometidos en ofrecer una muchas actividades que perduran como base de nuestra identidad. Entre las actividades incluimos la enseñanza de natación, momentos recreativos, experiencias en contacto con la naturaleza y mucho más.</p>
                     </div>
                     <div className="p-acts-2 flex justify-center items-center text-xl px-4 sm:px-12">
-                        <p>A lo largo de años de dedicación y éxito, tuvimos la fortuna de sumar a muchos profesionales a nuestro equipo. Entre ellos, destacamos a la profe Serena Jacquet, quien se une a nosotros, el legado de Roxanna. No es solo una profesional más; Serena es nada menos que la hija de Roxanna, sumando a la pasión y el compromiso que caracterizan a nuestra institución</p>
+                        <p>A lo largo de años de dedicación y éxito, tuvimos la fortuna de sumar a muchos profesionales a nuestro equipo. Entre ellos, destacamos a la seño Serena Jacquet, quien se une a nosotros, el legado de Roxanna. No es solo una profesional más; Serena es nada menos que la hija de Roxanna, sumando a la pasión y el compromiso que caracterizan a nuestra institución</p>
                     </div>
                     <div className="img-act-2 m-[auto]">
                         {/* Pasamos por props el indice , el estado, y las funciones para ir cambiando las imagenes */}
